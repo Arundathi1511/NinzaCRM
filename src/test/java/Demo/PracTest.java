e2f5088f3ab1d5d14bd86dd9ad78c30e65fd5f36
91949 <91949@LAPTOP-TLHFIKHS>
2025-12-18 07:44:35 +0530

    First time committed

---
+++ src/test/java/Demo/PracTest.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class PracTest {
+
+	@Test(groups="regression")
+	public void BikeTest()
+	{
+		System.out.println("Yamaha");
+	}
+
+}
